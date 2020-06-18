86e0c81a54d73fc6e2a9345b7ca07fd06ea72ea8
gautamsumu <gautamsumu@gmail.com>
2020-06-18 19:29:18 +0530

    merge sort and quick sort

--- c++/sorting.h
+++ c++/sorting.h
@@ -7,9 +7,11 @@ using namespace std;
 class Sorting{
 	public:
 		static int available_sorting_choices(){
-			int algorithms_implemented = 2;
+			int algorithms_implemented = 4;
 			cout << "1 - Selection Sort\n";
-			cout << "1 - Bubble Sort\n";
+			cout << "2 - Bubble Sort\n";
+			cout << "3 - Merge Sort\n";
+			cout << "4 - Quick Sort\n";
 			return algorithms_implemented;
 		}
 
@@ -20,6 +22,12 @@ class Sorting{
 					break;
 				case 2:
 					bubble_sort(numbers);
+					break;
+				case 3:
+					merge_sort(numbers);
+					break;
+				case 4:
+					quick_sort(numbers);
 					break;
 			}
 		}
@@ -52,6 +60,74 @@ class Sorting{
 				}
 			}
 		}
+
+		static void merge_sort(vector<int> &numbers){
+			merge_sort_helper(numbers, 0, numbers.size());
+		}
+
+		static void merge_sort_helper(vector<int> &numbers, int left, int right){
+			int mid = (left + right) / 2;
+			if(left < mid){
+				merge_sort_helper(numbers, left, mid);
+				merge_sort_helper(numbers, mid, right);
+				merge(numbers, left, mid, right);
+			}
+		}
+
+		static void merge(vector<int> &numbers, int left, int mid, int right){
+			int leftSize = mid - left;
+			int rightSize = right - mid;
+			vector<int> tmpLeft(leftSize), tmpRight(rightSize);
+			for(int i = 0; i < leftSize; ++i){
+				tmpLeft[i] = numbers[left + i];
+			} 
+			for(int j = 0; j < rightSize; ++j){
+				tmpRight[j] = numbers[mid + j];
+			}
+			int leftIndex = 0;
+			int rightIndex = 0;
+			for(int mainIndex = left; mainIndex < right; ++mainIndex){
+				if(leftIndex < leftSize && rightIndex < rightSize){
+					if(tmpLeft[leftIndex] <= tmpRight[rightIndex]){
+						numbers[mainIndex] = tmpLeft[leftIndex];
+						++leftIndex;
+					}
+					else{
+						numbers[mainIndex] = tmpRight[rightIndex];
+						++rightIndex;
+					}
+				}
+				else if(leftIndex < leftSize){
+					numbers[mainIndex] = tmpLeft[leftIndex];
+					++leftIndex;
+				}
+				else{
+					numbers[mainIndex] = tmpRight[rightIndex];
+					++rightIndex;
+				}
+			}
+		}
+
+		static void quick_sort(vector<int> &numbers){
+			quick_sort_helper(numbers, 0, numbers.size() - 1);
+		}
+
+		static void quick_sort_helper(vector<int> &numbers, int left, int right){
+			if(left < right){
+				int tail = left;
+				for(int head = left; head < right; ++head){
+					if(numbers[head] < numbers[right]){
+						Utils::swap(numbers[tail], numbers[head]);
+						++tail;
+					}
+				}
+				Utils::swap(numbers[tail], numbers[right]);
+				
+				quick_sort_helper(numbers, left, tail - 1);
+				quick_sort_helper(numbers, tail + 1, right);
+			}
+		}
+
 };
 
 
